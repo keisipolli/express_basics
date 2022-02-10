f3f9bce0ff37dea67a119fdf554fb4c18cdf6e83
Keisi Polli <keisi.polli@khk.ee>
2022-02-10 15:15:03 +0200

    template loop use

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const path = require('path')
+const express = require('express')
 const app = express();
+const path = require('path')
 
-app.use(express.static('public'))
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('views engine', 'ejs');
-app.get('/user/:username', (req, res)=> {
-    let user = req.params.username;
-    res.render('index', {username : user});
-});
+
+app.get('/questions', (req, res)=> {
+    let questions = [
+        {title: "What is node.js?", user: "Kadi", votes: "10"},
+        {title: "What is express.js?", user: "Mike", votes: "8"}
+    ]
+    res.render('index', {questions : questions});
 
 // listen app via port
 app.listen(3000, ()=> {
